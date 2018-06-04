5a994b16282574788b13ef80da5bd9ff7ab48f2a
spiderPan <panbanglanfeng@gmail.com>
2018-06-03 21:05:14 -0400

    added download functions and data folders

--- 1_notmnist.py
+++ 1_notmnist.py
@@ -5,9 +5,48 @@ import numpy as np
 import os
 import sys
 import tarfile
-import IPython.display
-import display, Image
+from IPython.display import display, Image
 from scipy import ndimage
 from sklearn.linear_model import LogisticRegression
 from six.moves.urllib.request import urlretrieve
 from six.moves import cPickle as pickle
+
+url = 'http://commondatastorage.googleapis.com/books1000/'
+last_percent_reported = None
+data_root = 'data'
+
+
+def download_progress_hook(count, blockSize, totalSize):
+    global last_percent_reported
+    percent = int(count * blockSize * 100 / totalSize)
+
+    if last_percent_reported != percent:
+        if percent % 5 == 0:
+            sys.stdout.write("%s%%" % percent)
+            sys.stdout.flush()
+        else:
+            sys.stdout.write('.')
+            sys.stdout.flush()
+
+        last_percent_reported = percent
+
+
+def maybe_download(filename, expected_bytes, force=False):
+    dest_filename = os.path.join(data_root, filename)
+    print(dest_filename)
+    if force or not os.path.exists(dest_filename):
+        print('Attempt8ing to download:', filename)
+        filename, _ = urlretrieve(url + filename, dest_filename, reporthook=download_progress_hook)
+        print('Download Completed!')
+    statinfo = os.stat(dest_filename)
+    print(statinfo)
+    if statinfo.st_size == expected_bytes:
+        print('Found and verified', dest_filename)
+    else:
+        raise Exception('Failed to verify ' + dest_filename + '. Can you get to ii with a browser?')
+
+    return dest_filename
+
+
+train_filename = maybe_download('notMNIST_large.tar.gz', 247336696)
+test_filename = maybe_download('notMNIST_small.tar.gz', 8458043)
