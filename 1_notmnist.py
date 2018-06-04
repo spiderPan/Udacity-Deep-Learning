75c05adee9d379dd7ee0579eac2447341d75c52e
spiderPan <panbanglanfeng@gmail.com>
2018-06-03 20:21:36 -0400

    added all packages

---
+++ 1_notmnist.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+import tarfile
+import IPython.display
+import display, Image
+from scipy import ndimage
+from sklearn.linear_model import LogisticRegression
+from six.moves.urllib.request import urlretrieve
+from six.moves import cPickle as pickle
