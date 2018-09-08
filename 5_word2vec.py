f9599e9e3577549961cad926f122eb7a0fa72b46
spiderPan <panbanglanfeng@gmail.com>
2018-09-08 13:32:05 -0400

    generate batch

--- 5_word2vec.py
+++ 5_word2vec.py
@@ -55,6 +55,36 @@ def build_dataset(words):
     return data, count, dictionary, reverse_dictionary
 
 
+data_index = 0
+
+
+def generate_batch(batch_size, num_skips, skip_window):
+    global data_index
+    assert batch_size % num_skips == 0
+    assert num_skips <= 2 * skip_window
+    batch = np.ndarray(shape=(batch_size), dtype=np.int32)
+    labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
+    span = 2 * skip_window + 1
+    buffer = collections.deque(maxlen=span)
+
+    for _ in range(span):
+        buffer.append(data[data_index])
+        data_index = (data_index + 1) % len(data)
+
+    for i in range(batch_size // num_skips):
+        target = skip_window
+        targets_to_avoid = [skip_window]
+        for j in range(num_skips):
+            while target in targets_to_avoid:
+                target = random.randint(0, span - 1)
+            targets_to_avoid.append(target)
+            batch[i * num_skips + j] = buffer[skip_window]
+            labels[i * num_skips + j, 0] = buffer[target]
+        buffer.append(data_index + 1)
+        data_index = (data_index + 1) % len(data)
+    return batch, labels
+
+
 filename = maybe_download('./data/text8.zip', 31344016)
 words = read_data(filename)
 print('Data size %d' % len(words))
@@ -63,3 +93,11 @@ data, count, dictionary, reverse_dictionary = build_dataset(words)
 print('Most common words (+UNK)', count[:5])
 print('Sample data', data[:10])
 
+print('data:', [reverse_dictionary[di] for di in data[:8]])
+
+for num_skips, skip_window in [(2, 1), (4, 2)]:
+    data_index = 0
+    batch, labels = generate_batch(batch_size=8, num_skips=num_skips, skip_window=skip_window)
+    print('\nwith num_skips = %d and skip_window = %d:' % (num_skips, skip_window))
+    print('  batch:', [reverse_dictionary[bi] for bi in batch])
+    print('  labels:', [reverse_dictionary[li] for li in labels.reshape(8)])
